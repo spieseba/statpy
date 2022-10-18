2c6a86c2a4eec513a424dff254c61c8e11c5e39e
Sebastian Spiegel <sebastian@wlanhome112034.uni-regensburg.de>
2022-10-18 13:39:01 +0200

    add covariance estimator, data axis

--- statistics/jackknife.py
+++ statistics/jackknife.py
@@ -10,11 +10,19 @@ class jackknife():
         pass
 
     def sample(self, idx):
-        return np.delete(self.x, idx)
+        return np.delete(self.x, idx, axis=0)
     
     def variance(self, f):
         f_x = f(self.x)
         return np.mean([ (f(np.delete(self.x, k, axis=0)) - f_x)**2 for k in range(self.N)], axis=0) * (self.N - 1)
+
+    def covariance(self, f, return_samples=False):
+        f_x = f(self.x)
+        def outer_prod(a):
+            return np.outer(a,a)
+        if return_samples:
+            return np.array([outer_prod( (f(np.delete(self.x, k, axis=0)) - f_x) ) for k in range(self.N)]) * (self.N - 1)
+        return np.mean([outer_prod( (f(np.delete(self.x, k, axis=0)) - f_x) ) for k in range(self.N)], axis=0) * (self.N - 1) 
 
     # fast version for functions of means
     def variance_mean(self, f):
@@ -24,8 +32,8 @@ class jackknife():
 
     # fast version for functions of means
     def covariance_mean(self, f):
-        x_mean = np.mean(self.x)
+        x_mean = np.mean(self.x, axis=0)
         f_mean = f(x_mean)
         def outer_prod(a):
             return np.outer(a,a)
-        return np.mean( outer_prod( f( (self.N * x_mean - self.x[k]) / (self.N - 1) ) - f_mean  ) for k in range(self.N) )  * (self.N - 1)        
+        return np.mean( [outer_prod( f( (self.N * x_mean - self.x[k]) / (self.N - 1) ) - f_mean  ) for k in range(self.N)], axis=0)  * (self.N - 1)        
