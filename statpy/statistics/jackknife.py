4c2da6f534dfba314718b235cb89e4f3305ff61c
Sebastian Spiegel <sps53327@pc1012253082.uni-regensburg.de>
2022-12-19 13:35:11 +0100

    include weights in jackknife

--- statpy/statistics/jackknife.py
+++ statpy/statistics/jackknife.py
@@ -2,30 +2,35 @@ import numpy as np
 
 ################################################ functions of means ################################################
 
-def samples(f, x):
+def samples(f, x, weights=None):
     N = len(x)
-    mean = np.mean(x, axis=0)
-    return np.array([ f(mean + (mean - x[j]) / (N-1)) for j in range(N)])
+    if weights == None:
+        mean = np.mean(x, axis=0)
+        return np.array([ f(mean + (mean - x[j]) / (N-1)) for j in range(N)])
+    else:
+        s = []
+        for j in range(N):
+            xj = np.delete(x, j, axis=0)
+            wj = np.delete(weights, j, axis=0); wj = wj / np.mean(wj) # renormalize weights. This is a choice.
+            s.append( f(np.mean(wj * xj, axis=0)) )
+        return np.array(s)
 
-def variance(f, x):
+def variance(f, x, f_samples=None):
     N = len(x)
     mean = np.mean(x, axis=0)
     f_mean = f(mean)
-    return np.mean([ ( f( (N * mean - x[k]) / (N - 1) ) - f_mean )**2 for k in range(N) ], axis=0) * (N - 1)
+    if f_samples == None:
+        return np.mean([ ( f( (N * mean - x[k]) / (N - 1) ) - f_mean )**2 for k in range(N) ], axis=0) * (N-1)
+    return np.sum(np.array([(f_samples[j] - f_mean) for j in range(N)]), axis=0) * (N-1) / N   #np.mean((f_samples - f_mean)**2, axis=0) * (N-1)
 
-def covariance(f, x):
+def covariance(f, x, f_samples=None):
     N = len(x)
     mean = np.mean(x, axis=0)
     f_mean = f(mean)
-    def outer_prod(a):
-        return np.outer(a,a)
-    return np.mean( [outer_prod( f( (N * mean - x[k]) / (N - 1) ) - f_mean  ) for k in range(N)], axis=0) * (N - 1) 
-
-def covariance_samples(f, x, f_samples):
-    N = len(x)
-    f_mean = f(np.mean(x, axis=0))
     def outer_sqr(a):
         return np.outer(a,a)
+    if f_samples == None:
+        return np.mean( [outer_sqr( f( (N * mean - x[k]) / (N - 1) ) - f_mean  ) for k in range(N)], axis=0) * (N-1) 
     return np.sum(np.array([outer_sqr(f_samples[j] - f_mean) for j in range(N)]), axis=0) * (N-1) / N   
 
 def covariance_samples2(f_mean, f_samples):
@@ -36,10 +41,11 @@ def covariance_samples2(f_mean, f_samples):
 
 ################################################ arbitrary functions ################################################
 
-def variance_general(f, x):
-    N = len(x)
-    f_x = f(x)
-    return np.mean([ (f(np.delete(x, k, axis=0)) - f_x)**2 for k in range(N)], axis=0) * (N - 1)
+def variance_general(f, x, f_samples=None):
+    if f_samples == None:
+        N = len(x)
+        f_x = f(x)
+        return np.mean([ (f(np.delete(x, k, axis=0)) - f_x)**2 for k in range(N)], axis=0) * (N-1)
         
 def covariance_general(f, x):
     N = len(x)
