46a1389eeddc84f4ad78069599301dbe17414046
Sebastian Spiegel <sebastian@wlanhome183052.uni-regensburg.de>
2022-12-19 14:59:09 +0100

    implement new version of binning including the possible use of weights

--- statpy/statistics/binning.py
+++ statpy/statistics/binning.py
@@ -1,16 +1,29 @@
 import numpy as np
 
-# produce binned copy of data ensemble with bins of size binsize 
-def bin(data, binsize):
-    data_binned = [] 
-    for i in range(0, len(data), binsize):
-        data_avg = 0; append_bin = True
-        for j in range(0,binsize):
-            try:
-                data_avg += data[i+j]
-            except IndexError:
-                append_bin = False
-                continue
-        if append_bin:
-            data_binned.append(data_avg / binsize)
-    return np.array(data_binned)
+def bin(data, b, *argv):
+    if len(argv) != 0:
+        w = argv[0]
+    Nb = len(data) // b # cuts off data which do not build a complete bin
+    bata = []
+    for i in range(Nb):
+        if len(argv) != 0:
+            mean = np.average(data[i*b:(i+1)*b], weights=w[i*b:(i+1)*b], axis=0)
+        else:
+            mean = np.mean(data[i*b:(i+1)*b], axis=0)
+        bata.append(mean)
+    return np.array(bata) 
+
+#def bin(data, binsize):
+#    data_binned = [] 
+#    for i in range(0, len(data), binsize):
+#        data_avg = 0; append_bin = True
+#        for j in range(0,binsize):
+#            try:
+#                data_avg += data[i+j]
+#            except IndexError:
+#                append_bin = False
+#                continue
+#        if append_bin:
+#            data_binned.append(data_avg / binsize)
+#    return np.array(data_binned)
+
