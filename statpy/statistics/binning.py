83367a0ea09f93e53b291c1ae712dc8d08d42e8e
Sebastian Spiegel <sebastian@wlanhome183052.uni-regensburg.de>
2022-12-19 14:22:57 +0100

    get rid of binned_stdErrs

--- statpy/statistics/binning.py
+++ statpy/statistics/binning.py
@@ -14,11 +14,3 @@ def bin(data, binsize):
         if append_bin:
             data_binned.append(data_avg / binsize)
     return np.array(data_binned)
-
-
-def binned_stdErrs(data, binsize_max):
-    stdErr_binned = []
-    for binsize in range(1, binsize_max):
-        data_binned = bin(data, binsize)
-        stdErr_binned.append( np.std(data_binned, ddof=1) / np.sqrt(len(data_binned)) )
-    return np.array(stdErr_binned)
