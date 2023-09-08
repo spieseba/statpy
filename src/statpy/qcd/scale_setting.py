3e938a3a76c5c4c681c6f76155aaf234c942fd36
Sebastian Spiegel <sebastian.spiegel@physik.uni-regensburg.de>
2023-09-08 15:24:06 +0200

    restructure scale setting and correct naming

--- src/statpy/qcd/scale_setting.py
+++ src/statpy/qcd/scale_setting.py
@@ -35,84 +35,73 @@ class gradient_flow_scale:
             return ( 0.25 * np.roll(f, 2) - 2.0 * np.roll(f, 1) + 2.0 * np.roll(f, -1) - 0.25 * np.roll(f, -2) ) / (3.0 * dx)
 
     ########################################## t0 ###############################################
-    
-    def comp_t2E(self, tau, E):
+
+    def compute_t2E(self, tau, E):
         return tau**2. * E
     
-    def comp_sqrt_tau0(self, tau, t2E):
+    def compute_sqrt_tau0(self, tau, E):
+        t2E = self.compute_t2E(tau, E)
         return np.sqrt(self.linear_interpolation(tau, t2E, self.ct0))
     
-    def comp_sqrt_t0_GeV(self, sqrt_tau0, sqrt_t0_fm):
-        return sqrt_tau0 * 0.1973 / sqrt_t0_fm
-    
-    def set_sqrt_tau0(self, tau, E):
-        t2E = self.comp_t2E(tau, E)
-        sqrt_tau0 = self.comp_sqrt_tau0(tau, t2E)
-        return sqrt_tau0
-
-    def set_sqrt_t0_GeV(self, tau, E, sqrt_t0_fm=None):
-        if sqrt_t0_fm == None:
+    # sqrt_t0 = 0.1638 fm, sqrt_tau0 = sqrt_t0 / a ==> a[fm] = sqrt_t0 / sqrt_tau0
+    def compute_a_fm_from_sqrt_tau0(self, sqrt_tau0, sqrt_t0_fm=None):
+        if sqrt_t0_fm is None:
             sqrt_t0_fm = self.sqrt_t0_fm
-        t2E = self.comp_t2E(tau, E)
-        sqrt_tau0 = self.comp_sqrt_tau0(tau, t2E)
-        return self.comp_sqrt_t0_GeV(sqrt_tau0, sqrt_t0_fm)
+        return sqrt_t0_fm / sqrt_tau0
 
+    # 0.1973 GeV fm = 1, a = X fm ==> a^-1 = 1/ (X fm) = 0.1973 / (X 0.1973 fm) = 0.1973 GeV / X  
+    def compute_a_inv_GeV_from_sqrt_tau0(self, sqrt_tau0, sqrt_t0_fm=None):
+        return 0.1973 / self.compute_a_fm_from_sqrt_tau0(sqrt_tau0, sqrt_t0_fm)
+    
     ########################################## w0 ###############################################
     
-    def comp_tdt2E(self, tau, E):
-        t2E = self.comp_t2E(tau, E)
+    def compute_tdt2E(self, tau, E):
+        t2E = self.compute_t2E(tau, E)
         return tau * self.derivative(f=t2E, dx=(tau[1]-tau[0]))
 
-    def comp_omega0(self, tau, tdt2E):
+    def compute_omega0(self, tau, E):
+        tdt2E = self.compute_tdt2E(tau, E)
         return np.sqrt(self.linear_interpolation(tau[1:-1], tdt2E[1:-1], self.cw0))
-    
-    def comp_w0_GeV(self, omega0, w0_fm):
-        return omega0 * 0.1973 / w0_fm
-
-    def set_omega0(self, tau, E):
-        tdt2E = self.comp_tdt2E(tau, E)
-        return self.comp_omega0(tau, tdt2E)
-
-    def set_w0_GeV(self, tau, E, w0_fm=None):
-        if w0_fm == None:
+     
+    # w0 = 0.1670 fm, omega0 = w0 / a ==> a[fm] = w0/omega0
+    def compute_a_fm_from_omega0(self, omega0, w0_fm=None):
+        if w0_fm is None:
             w0_fm = self.w0_fm
-        tdt2E = self.comp_tdt2E(tau, E)
-        omega0 = self.comp_omega0(tau, tdt2E)
-        return omega0 * 0.1973 / w0_fm
+        return w0_fm / omega0
+
+    # 0.1973 GeV fm = 1, a = X fm ==> a^-1 = 1/ (X fm) = 0.1973 / (X 0.1973 fm) = 0.1973 GeV / X 
+    def compute_a_inv_GeV_from_omega0(self, omega0, w0_fm=None):
+        return 0.1973 / self.compute_a_fm_from_omega0(omega0, w0_fm)
     
+
 def db_local_gradient_flow_scale(db, leaf_prefix, scale_type="both"):
     assert scale_type in ["sqrt_tau0", "omega0", "both"]
     scale = gradient_flow_scale()
     if scale_type in ["sqrt_tau0", "both"]: 
-        db.combine_sample(leaf_prefix + "/tau", leaf_prefix + "/E", f=scale.set_sqrt_tau0, dst_tag=leaf_prefix + "/local_sqrt_tau0")
+        db.combine_sample(leaf_prefix + "/tau", leaf_prefix + "/E", f=scale.compute_sqrt_tau0, dst_tag=leaf_prefix + "/local_sqrt_tau0")
     if scale_type in ["omega0", "both"]:     
-        db.combine_sample(leaf_prefix + "/tau", leaf_prefix + "/E", f=scale.set_omega0, dst_tag=leaf_prefix + "/local_omega0")
+        db.combine_sample(leaf_prefix + "/tau", leaf_prefix + "/E", f=scale.compute_omega0, dst_tag=leaf_prefix + "/local_omega0")
 
 def db_gradient_flow_scale(db, leaf_prefix, binsize, verbose=True):
     tau = db.database[leaf_prefix + "/tau"].mean
     scale = gradient_flow_scale()
-    # tau0
-    db.combine(leaf_prefix + "/E", f=lambda x: scale.set_sqrt_tau0(tau, x), dst_tag=leaf_prefix + "/sqrt_tau0")
-    sqrt_tau0_var = db.jackknife_variance(leaf_prefix + "/sqrt_tau0", binsize)
-    # t0
-    db.combine(leaf_prefix + "/sqrt_tau0", f=lambda x: scale.comp_sqrt_t0_GeV(x, scale.sqrt_t0_fm), dst_tag=leaf_prefix + "/sqrt_t0_GeV") 
-    sqrt_t0_GeV_stat_var = db.jackknife_variance(leaf_prefix + "/sqrt_t0_GeV", binsize)
-    # propagate systematic error of t0
-    sqrt_t0_GeV_mean_shifted = scale.comp_sqrt_t0_GeV(db.database[leaf_prefix + "/sqrt_tau0"].mean, scale.sqrt_t0_fm + scale.sqrt_t0_fm_std)
-    db.propagate_sys_var(sqrt_t0_GeV_mean_shifted, dst_tag=leaf_prefix + "/sqrt_t0_GeV", sys_tag="sqrt_t0_GeV")
-    sqrt_t0_GeV_sys_var = db.database[leaf_prefix + "/sqrt_t0_GeV"].misc["SYS_VAR_sqrt_t0_GeV"]
-    # omega0
-    db.combine(leaf_prefix + "/E", f=lambda x: scale.set_omega0(tau, x), dst_tag=leaf_prefix + "/omega0")
-    omega0_var = db.jackknife_variance(leaf_prefix + "/omega0", binsize)
-    # w0
-    db.combine(leaf_prefix + "/omega0", f=lambda x: scale.comp_w0_GeV(x, scale.w0_fm), dst_tag=leaf_prefix + "/w0_GeV") 
-    w0_GeV_stat_var = db.jackknife_variance(leaf_prefix + "/w0_GeV", binsize)
-    # propagate systematic error of w0
-    w0_GeV_mean_shifted = scale.comp_w0_GeV(db.database[leaf_prefix + "/omega0"].mean, scale.w0_fm + scale.w0_fm_std)
-    db.propagate_sys_var(w0_GeV_mean_shifted, dst_tag=leaf_prefix + "/w0_GeV", sys_tag="w0_GeV")
-    w0_GeV_sys_var = db.database[leaf_prefix + "/w0_GeV"].misc["SYS_VAR_w0_GeV"]
+    ### sqrt_tau0 ###
+    db.combine(leaf_prefix + "/E", f=lambda x: scale.compute_sqrt_tau0(tau, x), dst_tag=leaf_prefix + "/sqrt_tau0")
+    # a_inv_GeV from sqrt_tau0
+    db.combine(leaf_prefix + "/sqrt_tau0", f=lambda x: scale.compute_a_inv_GeV_from_sqrt_tau0(x, scale.sqrt_t0_fm), dst_tag=leaf_prefix + "/sqrt_tau0/a_inv_GeV") 
+    # propagate systematic error 
+    a_inv_GeV_from_sqrt_t0_mean_shifted = scale.compute_a_inv_GeV_from_sqrt_tau0(db.database[leaf_prefix + "/sqrt_tau0"].mean, scale.sqrt_t0_fm + scale.sqrt_t0_fm_std)
+    db.propagate_sys_var(a_inv_GeV_from_sqrt_t0_mean_shifted, dst_tag=leaf_prefix + "/sqrt_tau0/a_inv_GeV", sys_tag="sqrt_t0")
     if verbose:
-        db.message(f"sqrt(tau0) = {db.database[leaf_prefix + '/sqrt_tau0'].mean:.4f} +- {sqrt_tau0_var**.5:.4f}")
-        db.message(f"omega0 = {db.database[leaf_prefix + '/omega0'].mean:.4f} +- {omega0_var**.5:.4f}")
-        db.message(f"sqrt(t0)/GeV (cutoff) = {db.database[leaf_prefix + '/sqrt_t0_GeV'].mean:.4f} +- {sqrt_t0_GeV_stat_var**.5:.4f} (STAT) +- {sqrt_t0_GeV_sys_var**.5:.4f} (SYS) [{(sqrt_t0_GeV_stat_var+sqrt_t0_GeV_sys_var)**.5:.4f} (STAT+SYS)]")
-        db.message(f"w0/GeV (cutoff) = {db.database[leaf_prefix + '/w0_GeV'].mean:.4f} +- {w0_GeV_stat_var**.5:.4f} (STAT) +- {w0_GeV_sys_var**.5:.4f} (SYS) [{(w0_GeV_stat_var+w0_GeV_sys_var)**.5:.4f} (STAT+SYS)]")
+        db.message(f"sqrt(tau0) = {db.database[leaf_prefix + '/sqrt_tau0'].mean:.4f} +- {db.jackknife_variance(leaf_prefix + '/sqrt_tau0', binsize)**.5:.4f}")
+        db.message(f"a_inv_GeV from sqrt(tau0) (cutoff) = {db.database[leaf_prefix + '/sqrt_tau0/a_inv_GeV'].mean:.4f} +- {db.jackknife_variance(leaf_prefix + '/sqrt_tau0/a_inv_GeV', binsize)**.5:.4f} (STAT) +- {db.get_sys_var(leaf_prefix + '/sqrt_tau0/a_inv_GeV')**.5:.4f} (SYS) [{db.get_tot_var(leaf_prefix + '/sqrt_tau0/a_inv_GeV', binsize)**.5:.4f} (STAT+SYS)]")
+    ### omega0 ###
+    db.combine(leaf_prefix + "/E", f=lambda x: scale.compute_omega0(tau, x), dst_tag=leaf_prefix + "/omega0")
+    # a_inv_GeV from omega0
+    db.combine(leaf_prefix + "/omega0", f=lambda x: scale.compute_a_inv_GeV_from_omega0(x, scale.w0_fm), dst_tag=leaf_prefix + "/omega0/a_inv_GeV")
+    # propagate systematic error 
+    a_inv_GeV_from_omega_mean_shifted = scale.compute_a_inv_GeV_from_omega0(db.database[leaf_prefix + "/omega0"].mean, scale.w0_fm + scale.w0_fm_std)
+    db.propagate_sys_var(a_inv_GeV_from_omega_mean_shifted, dst_tag=leaf_prefix + "/omega0/a_inv_GeV", sys_tag="w0")
+    if verbose:
+        db.message(f"omega0 = {db.database[leaf_prefix + '/omega0'].mean:.4f} +- {db.jackknife_variance(leaf_prefix + '/omega0', binsize)**.5:.4f}")
+        db.message(f"a_inv_GeV from omega0 (cutoff) = {db.database[leaf_prefix + '/omega0/a_inv_GeV'].mean:.4f} +- {db.jackknife_variance(leaf_prefix + '/omega0/a_inv_GeV', binsize)**.5:.4f} (STAT) +- {db.get_sys_var(leaf_prefix + '/omega0/a_inv_GeV')**.5:.4f} (SYS) [{(db.get_tot_var(leaf_prefix + '/omega0/a_inv_GeV', binsize))**.5:.4f} (STAT+SYS)]")
