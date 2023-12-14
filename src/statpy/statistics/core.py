5cac88a5d0bd9bf8f4c6132c6cb66815f7191bee
Sebastian Spiegel <sebastian.spiegel@physik.uni-regensburg.de>
2023-12-14 15:32:24 +0100

    remove obsolete imports

--- src/statpy/statistics/core.py
+++ src/statpy/statistics/core.py
@@ -2,8 +2,6 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from ..fitting.core import Fitter, model_prediction_var
-from iminuit import Minuit
 
 # standard error
 def ste(data, tau_int=0.5):
