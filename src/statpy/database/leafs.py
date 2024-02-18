e33e3c3c46cadcdcfddae99896aae073015b0607
Sebastian Spiegel <sebastian.spiegel@physik.uni-regensburg.de>
2024-02-18 17:10:32 +0100

    add checksum computation

--- src/statpy/database/leafs.py
+++ src/statpy/database/leafs.py
@@ -1,4 +1,4 @@
-import numpy as np
+import hashlib
 
 class Leaf:
     def __init__(self, mean, jks, sample, misc=None):
@@ -9,7 +9,18 @@ class Leaf:
 
     @classmethod
     def from_dict(cls, dct):
+        # verify checksum!
         return cls(**dct)
     
     def to_dict(self):
+        data = self._to_dict()
+        #data['checksum'] = self._calculate_checksum(data)
+        return data
+    
+    def _to_dict(self):
         return self.__dict__
+     
+    def _calculate_checksum(self, data):
+        hash_function = hashlib.sha1()  
+        hash_function.update(str(data).encode('utf-8'))  
+        return hash_function.hexdigest() 
