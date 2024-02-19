a25666a94cfddf4815e8e94764220bf7260a12c7
Sebastian Spiegel <sebastian.spiegel@physik.uni-regensburg.de>
2024-02-19 10:55:59 +0100

    add crc32 checksum to test for data corruption

--- src/statpy/database/leafs.py
+++ src/statpy/database/leafs.py
@@ -1,4 +1,6 @@
-import hashlib
+from ..log import message
+from . import custom_json as json
+import zlib
 
 class Leaf:
     def __init__(self, mean, jks, sample, misc=None):
@@ -7,20 +9,27 @@ class Leaf:
         self.sample = sample
         self.misc = misc
 
-    @classmethod
-    def from_dict(cls, dct):
-        # verify checksum!
-        return cls(**dct)
+    def _to_dict(self):
+        return {"mean": self.mean, "jks": self.jks, "sample": self.sample, "misc": self.misc}
     
     def to_dict(self):
         data = self._to_dict()
-        #data['checksum'] = self._calculate_checksum(data)
+        data['checksum'] = calculate_checksum(data)
         return data
+
+    @classmethod
+    def from_dict(cls, data):
+        cksum = data.pop('checksum', None)
+        if cksum:
+            cksumcomp = calculate_checksum(data)
+            if cksum == cksumcomp:
+                return cls(**data)
+            else:
+                raise Exception("Data corrupted!")
+        else:
+            message("Checksum missing. Data may be corrupt.") 
+
     
-    def _to_dict(self):
-        return self.__dict__
-     
-    def _calculate_checksum(self, data):
-        hash_function = hashlib.sha1()  
-        hash_function.update(str(data).encode('utf-8'))  
-        return hash_function.hexdigest() 
+def calculate_checksum(data):
+    data_json = json.dumps(data).encode('utf-8')  # Serialize as JSON first
+    return zlib.crc32(data_json) & 0xFFFFFFFF  # Ensure unsigned 32-bit
