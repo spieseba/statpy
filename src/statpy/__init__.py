56b353cde3d752c9471feb7db93ad77e498881eb
Sebastian Spiegel <sebastian.spiegel@physik.uni-regensburg.de>
2024-03-02 18:14:51 +0100

    fix imports

--- src/statpy/__init__.py
+++ src/statpy/__init__.py
@@ -1,6 +1,6 @@
-from statpy.database.core import DB
-from statpy.log import message
-
 import statpy.fitting
 import statpy.qcd
 import statpy.statistics
+
+from statpy.database.core import DB
+from statpy.log import message
---
+++ src/statpy/statistics/__init__.py
@@ -0,0 +1,4 @@
+import statpy.statistics.auto
+import statpy.statistics.bootstrap
+import statpy.statistics.core
+import statpy.statistics.jackknife
