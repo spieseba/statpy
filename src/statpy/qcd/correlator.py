47fd7e9136fcaecbef7044119a978c0b43762e46
Sebastian Spiegel <sebastian.spiegel@physik.uni-regensburg.de>
2024-04-18 12:51:39 +0200

    use best parameter from last fit range for p0 when guess gives NaN; try to prevent symmetric fitting

--- src/statpy/qcd/correlator.py
+++ src/statpy/qcd/correlator.py
@@ -296,6 +296,10 @@ class LatticeCharmToolkit():
             m1_eff = effective_mass(Ct_excited)[fit_range[0]]
             A1_eff = effective_amplitude(Ct_excited, m1_eff)[fit_range[0]]
         message(f"guessed p0 = [{A0_eff}, {m0_eff},  {A1_eff}, {m1_eff}]")
+        if m1_eff < 1.2 * m0_eff:
+            message("guess for excited state mass too small: p0[2] = abs(p0[2]); p0[3] = 2*p0[1]")
+            A1_eff = abs(A1_eff); m1_eff = 2. * m0_eff
+            message(f"---> [{A0_eff}, {m0_eff},  {A1_eff}, {m1_eff}]")
         return np.array([A0_eff,m0_eff,A1_eff,m1_eff])
 
     def fit_range_fit(self, tag, binsize, initial_fit_ranges, p0, fit_model, verbosity):
@@ -325,6 +329,9 @@ class LatticeCharmToolkit():
                          "double-sinh": lambda t,p,y: double_sinh_chi2(t, p, y, W, Nt),
                          "double-exp": lambda t,p,y: double_exp_chi2(t, p, y, W)}[fit_model]
             p0_tmp = self.get_p0_guess(tag, binsize, fit_model, t) if p0 is None else p0
+            if np.isnan(p0_tmp).any():
+                p0_tmp = fit_range_dict["mean"] if fit_range_dict["mean"] is not None else np.ones(len(p0_tmp))
+                message(f"p0 guess contains NaN, use fit result from previous fit range if available, else default to ones: {p0_tmp}")
             try:
                 best_parameter, best_parameter_jks, misc = fit(self.db, t, binned_tag, p0_tmp, chi2_func, self.fit_method, self.fit_params, self.res_fit_method, self.res_fit_params)
                 misc["fit_model"] = fit_model
