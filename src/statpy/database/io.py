6def8fbf0ef85cfeb1fcf22ed3afdd9152012c1c
Sebastian Spiegel <sebastian.spiegel@physik.uni-regensburg.de>
2024-06-17 15:13:14 +0200

    fix bug in load CLS for case when there is a mismatch between rwf and hdf5 configs

--- src/statpy/database/io.py
+++ src/statpy/database/io.py
@@ -10,26 +10,28 @@ def load_CLS(fn, rwf_fn, tags, branch_tag, exclude_SRCPOS=True, verbosity=0, acc
     message(f"reweighting factors: {rwf_fn}")
     message(f"tags: {tags}")
     message(f"store as branch tag: {branch_tag}")
-    message(f"---------------------------------")
     # data
     f = h5py.File(fn, "r")
     f_cfgs = np.array([int(cfg.decode("utf-8").split("n")[1]) for cfg in f.get("configlist")])
+    message(f"# of cfgs in hdf5 file: {len(f_cfgs)}")
     db = DB(verbosity=verbosity)
     # rwfs
     if rwf_fn is None:
-        cfgs = f_cfgs 
-        rwf = {f"{branch_tag}-{cfg}":1.0 for cfg in f_cfgs}
+        message(f"rwf file not available. Use rwf=1.0 for all configs.")
+        common_cfgs = f_cfgs
+        rwf = {f"{branch_tag}-{cfg}":1.0 for cfg in common_cfgs}
     else:
         assert os.path.isfile(rwf_fn) 
         rwf_cfgs = np.array(np.loadtxt(rwf_fn)[:,0], dtype=int)
-        cfgs = np.array([cfg for cfg in rwf_cfgs if cfg in f_cfgs])
-        message(f"#cfgs in hdf5: {len(f_cfgs)} | #cfgs in rwf: {len(rwf_cfgs)}")
-        if len(rwf_cfgs) != len(cfgs):
-            message(f"WARNING: rwf file has different number of configs than hdf5 file!")
-            if not accept_cfg_mismatch: sys.exit(1)
-            message(f"Use only configs that are in both files.")
+        common_cfgs = np.array([cfg for cfg in rwf_cfgs if cfg in f_cfgs])
+        message(f"# of cfgs in rwf file: {rwf_cfgs.shape[0]} | # of common configs with hdf5 file : {common_cfgs.shape[0]}")
+        if rwf_cfgs.shape[0] != common_cfgs.shape[0] or rwf_cfgs.shape[0] != f_cfgs.shape[0]:
+            message(f"WARNING: rwf file has different configs than hdf5 file!")
+            if not accept_cfg_mismatch: 
+                sys.exit(1)
+            message(f"---> Add only common configs to database.")
         rwf = np.loadtxt(rwf_fn)[:,1] 
-        rwf = {f"{branch_tag}-{cfg}":val for cfg, val in zip(cfgs, rwf[rwf_cfgs-1])} 
+        rwf = {f"{branch_tag}-{cfg}":val for cfg,val in zip(rwf_cfgs, rwf) if cfg in common_cfgs} 
     db.add_leaf(tag=f"{branch_tag}/rwf", mean=None, jks=None, sample=rwf, misc=None)
     db.add_nrwf(rwf_tag=f"{branch_tag}/rwf")
     # data
@@ -38,8 +40,8 @@ def load_CLS(fn, rwf_fn, tags, branch_tag, exclude_SRCPOS=True, verbosity=0, acc
             if t in key:
                 if "SRCPOS" in key and exclude_SRCPOS:
                     continue
-                vals = f.get(key)
-                assert len(vals) == len(cfgs), "Missmatch between determined number of configs and stored values"
-                sample = {f"{branch_tag}-{cfg}":val for cfg, val in zip(cfgs, vals)}
+                f_vals = np.array(f.get(key))
+                sample = {f"{branch_tag}-{cfg}":val for cfg,val in zip(f_cfgs, f_vals) if cfg in common_cfgs}
                 db.add_leaf(tag=f"{branch_tag}/{key}", mean=None, jks=None, sample=sample, misc=None, verbosity=verbosity)
+    message(f"---------------------------------")
     return db
