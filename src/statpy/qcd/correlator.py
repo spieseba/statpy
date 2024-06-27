c74ca11dc5f0296f617406bef2c76d9adac8d8bd
Sebastian Spiegel <sebastian.spiegel@physik.uni-regensburg.de>
2024-06-27 17:46:05 +0200

    maximum signal to noise ration from correlator not max

--- src/statpy/qcd/correlator.py
+++ src/statpy/qcd/correlator.py
@@ -292,11 +292,10 @@ class LatticeCharmToolkit():
         message(f"Fold correlator {Ct_tag}.")
         self.db.combine_sample(Ct_tag, f=lambda Ct: self._fold(Ct, antiperiodic), dst_tag=f"{Ct_tag}/folded")
 
-    def get_folded_tmax(self, Ct_tag, max_val=50):
+    def get_folded_tmax(self, Ct_tag, max_val=100):
         message(f"Determine tmax by signal to noise ratio < {max_val}.")
-        mt, mt_jks, _ = self.db.combine(Ct_tag, f=effective_mass_acosh1) # spectrum paper method does not make much sense here as it involces Ct(Nt//2)
-        signal_to_noise = mt / jackknife.variance(self.db.as_array(mt_jks))**.5
-        tmax = next((i for i, x in enumerate(signal_to_noise) if x < max_val), -1)
+        signal_to_noise = self.db.database[Ct_tag].mean / self.db.jackknife_variance(Ct_tag)**.5
+        tmax = next((i for i, x in enumerate(signal_to_noise) if (i > 10) and (x < max_val)), -1)
         assert tmax != -1, f"tmax could not be found. signal to noise: {signal_to_noise}"
         return tmax
 
