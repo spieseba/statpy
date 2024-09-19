3f7b3cfc1dd769fc2c4db0e0cfa3a1502ee8cf84
Sebastian Spiegel <sebastian.spiegel@physik.uni-regensburg.de>
2024-09-19 16:47:44 +0200

    rework determination of tmax

--- src/statpy/qcd/correlator.py
+++ src/statpy/qcd/correlator.py
@@ -298,17 +298,7 @@ class LatticeCharmToolkit():
     def fold_correlator(self, Ct_tag, antiperiodic=False):
         message(f"Fold correlator {Ct_tag}.")
         self.db.combine_sample(Ct_tag, f=lambda Ct: self._fold(Ct, antiperiodic), dst_tag=f"{Ct_tag}/folded")
-
-    def get_tmax_signal_to_noise(self, Ct_tag, min_stn_val=100):
-        message(f"Determine tmax by signal to noise ratio < {min_stn_val}.")
-        signal_to_noise = self.db.database[Ct_tag].mean / self.db.jackknife_variance(Ct_tag)**.5
-        tmax = next((i for i, x in enumerate(signal_to_noise) if (i > 10) and (x < min_stn_val)), -1)
-        if tmax == -1:
-            message(f"Signal to noise ratio < {min_stn_val} could not be found. return tmax = -1")
-            message(f"Signal to noise ratio: {signal_to_noise}")
-        message(f"Found tmax = {tmax}.")
-        return tmax
-
+    
     def _fold(self, arr, antiperiodic=False):
         half = len(arr) // 2
         arr0 = arr[:half]
@@ -316,6 +306,30 @@ class LatticeCharmToolkit():
         if antiperiodic: arr1 *= -1.
         arr1[0] = arr0[0]
         return np.mean([arr0, arr1], axis=0)
+
+    def get_tmax_signal_to_noise(self, mean, var, min_stn_val=100, debug=False):
+        signal_to_noise = mean / var**.5 #self.db.database[Ct_tag].mean / self.db.jackknife_variance(Ct_tag)**.5
+        tmax = next((i for i, x in enumerate(signal_to_noise) if (i > 15) and (x < min_stn_val)), -1)
+        if tmax == -1:
+            tmax = len(mean) #len(self.db.database[Ct_tag].mean)
+            message(f"--- Signal to noise ratio never smaller than {min_stn_val} -> return tmax = len(mt) = {tmax}")
+        else:
+            message(f"--- Found tmax = {tmax} -> can use first {tmax} time slices")
+        if debug: message(f"--- Signal to noise ratios: {signal_to_noise}")
+        return tmax
+    
+    def get_tmax_from_deviation(self, mt_mean, mt_var, tmax_stn, n, debug=False):
+        message(f"--- Check whether m(t+1) in [m(t) - n sigma, m(t) + n sigma] with n = {n} up to t = {tmax_stn}.")
+        diff_in_sigma = abs(mt_mean - np.roll(mt_mean,+1)) / (n * mt_var**.5)
+        is_in_range = (diff_in_sigma < 1)[:tmax_stn]
+        tmax = next((i for i,x in enumerate(is_in_range) if not x and (i > 15)), -1)
+        if tmax == -1:
+            tmax = tmax_stn
+            message(f"--- m(t+1) lies always in [m(t) - n sigma, m(t) + n sigma] with n = {n} up to t = {tmax_stn} -> return tmax = {tmax}")
+        else:
+            message(f"--- Found tmax = {tmax} -> can use first {tmax} time slices")
+        if debug: message(f"--- abs(m(t) - m(t-1)) / (n*sigma) up to t = {tmax_stn}:\n {diff_in_sigma[:tmax_stn]}")
+        return tmax
 
     # determine improved PSA4 according to https://arxiv.org/pdf/1502.04999.pdf
     def determine_PSA4I(self, tag_PSPS_sml, tag_PSA4_sml, beta):
