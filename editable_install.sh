b4118696aa2e361dbf28805cf720b3a2b4810fd8
Sebastian Spiegel <sebastian.spiegel@physik.uni-regensburg.de>
2024-10-01 15:10:47 +0200

    install meson-python meson and ninja directly when running editable_install.sh

--- editable_install.sh
+++ editable_install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-python3 -m pip install --no-build-isolation --editable .
 # requires python packages: meson-python meson ninja as build dependencies need to be available when importing lib
+python3 -m pip install meson-python meson ninja
+python3 -m pip install --no-build-isolation --editable .
