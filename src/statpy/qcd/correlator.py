81b6c747def4ed7b1f9c545d026c3aee5956b82e
Sebastian Spiegel <sebastian.spiegel@physik.uni-regensburg.de>
2024-10-02 15:05:56 +0200

    add tbulk and tsrcs to averaged leafs

--- src/statpy/qcd/correlator.py
+++ src/statpy/qcd/correlator.py
@@ -271,7 +271,8 @@ class LatticeCharmToolkit():
         tmax_fw, tmax_bw = self._get_tmax_fw_bw(tsrcs_in_bulk, tbulk) # these values can be used directly for time slices
         for src_idx, Ct_tag in enumerate(Ct_tags_in_bulk):
             self.db.combine_sample(Ct_tag, f=lambda Ct: self._get_masked_Ct(Ct, tmax_fw[src_idx], tmax_bw[src_idx], antiperiodic), dst_tag=f"{Ct_tag}/masked")
-        self.db.combine_sample(*[f"{Ct_tag}/masked" for Ct_tag in Ct_tags_in_bulk], f=lambda *Cts_ma: np.ma.concatenate(Cts_ma, axis=0).mean(axis=0).compressed(), dst_tag=dst_tag)
+        combined_sample = self.db.combine_sample(*[f"{Ct_tag}/masked" for Ct_tag in Ct_tags_in_bulk], f=lambda *Cts_ma: np.ma.concatenate(Cts_ma, axis=0).mean(axis=0).compressed())
+        self.db.add_leaf(tag=dst_tag, mean=None, jks=None, sample=combined_sample, misc={"tsrcs":tsrcs_in_bulk, "tbulk":tbulk, "antiperiodic":antiperiodic})
         for Ct_tag in Ct_tags_in_bulk:
             self.db.remove_leaf(f"{Ct_tag}/masked")
 
