ee8a1d1d17b3ac63e1025e4ceb7022813cfe3c7f
Sebastian Spiegel <sebastian.spiegel@ur.de>
2026-06-08 14:15:52 +0200

    Stop suppressing invalid-value warnings in jackknife sample/variance

--- src/statpy/statistics/jackknife.py
+++ src/statpy/statistics/jackknife.py
@@ -7,16 +7,14 @@ def sample(x, weights=None, f=lambda x: x):
     w = np.ones(N) if weights is None else weights
     if len(w) != N:
         raise ValueError(f"jackknife.sample: weights length {len(w)} != sample length {N}")
-    with np.errstate(invalid='ignore'):
-        mean = np.average(x, axis=0, weights=w)
-        N_w = np.sum(w)
-        return np.array([ f( mean + w[j] * (mean - x[j]) / (N_w - w[j]) ) for j in range(N)])
+    mean = np.average(x, axis=0, weights=w)
+    N_w = np.sum(w)
+    return np.array([ f( mean + w[j] * (mean - x[j]) / (N_w - w[j]) ) for j in range(N)])
 
 def variance(jks, mean=None):
     if mean is None: mean = np.mean(jks, axis=0)
     N = len(jks)
-    with np.errstate(invalid='ignore'):
-        return np.sum(np.array([(jks[j] - mean)**2 for j in range(N)]), axis=0) * (N-1) / N
+    return np.sum(np.array([(jks[j] - mean)**2 for j in range(N)]), axis=0) * (N-1) / N
 
 def covariance(jks, mean=None):
     if mean is None: mean = np.mean(jks, axis=0)
